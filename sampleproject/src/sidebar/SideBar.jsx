7bc6050e0296bf377edafdf8e8402fdda91d0903
aliha <alihajji315@gmail.com>
2024-03-30 11:56:57 +0000

    EDucation

--- sampleproject/src/sidebar/SideBar.jsx
+++ sampleproject/src/sidebar/SideBar.jsx
@@ -17,23 +17,7 @@ export default function SideBar() {
             Mathematics B sciences Baccalaureate 2016 - 2017
             </p>
 
-            <p>
-            Faculty of Sciences - TETOUAN 2021 - 2023 <br/>
-            master’s degree - computer engineering <br />
-            Faculty of Sciences - TETOUAN 2017 - 2021 <br />
-            Bachelor  of Fundamental Studies - mathematics and computer sciences <br />
-            Technical High School Moulay Youssef <br />
-            Mathematics B sciences Baccalaureate 2016 - 2017
-            </p>
 
-            <p>
-            Faculty of Sciences - TETOUAN 2021 - 2023 <br/>
-            master’s degree - computer engineering <br />
-            Faculty of Sciences - TETOUAN 2017 - 2021 <br />
-            Bachelor  of Fundamental Studies - mathematics and computer sciences <br />
-            Technical High School Moulay Youssef <br />
-            Mathematics B sciences Baccalaureate 2016 - 2017
-            </p>
         </div>
       
     </div>
